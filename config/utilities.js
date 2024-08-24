61ce57712632f695db7eece9c5ef212ce7de453a
Krauchelli <mekaku26shi@gmail.com>
2024-08-24 10:29:44 +0700

    chore: Add cors middleware for cross-origin resource sharing

--- config/utilities.js
+++ config/utilities.js
@@ -2,10 +2,12 @@ var logger = require('morgan');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var cors = require('cors');
 var dotenv = require('dotenv');
 
 module.exports = (app) => {
     dotenv.config();
+    app.use(cors());
     app.use(logger('dev'));
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
