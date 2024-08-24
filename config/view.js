4c2727c68f155998d045121da531912e851aa8f4
Krauchelli <mekaku26shi@gmail.com>
2024-08-24 11:13:11 +0700

    chore: Update view configuration to use ejs engine

--- config/view.js
+++ config/view.js
@@ -2,5 +2,6 @@ var path = require('path');
 
 module.exports = (app) => {
     app.set('views', path.join(__dirname,'..', 'views'));
+    app.engine("ejs", require("ejs").__express);
     app.set('view engine', 'ejs');
 };
